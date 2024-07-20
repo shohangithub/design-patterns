e25b14197f5bc2c27293572f4184382e07654816
Rafiul Islam <sohan@redlimesolutions.com>
2024-07-20 16:23:32 +0600

    add problem for strategy problem

---
+++ Strategy-Problem/PaymentGateway.cs
@@ -0,0 +1,27 @@
+﻿namespace Strategy_Problem;
+
+internal class PaymentGateway
+{
+    public void Payment(string PaymentMethod)
+    {
+        // --- BKASH, NAGAD, ROCKET  ---
+
+        Console.WriteLine("proccessing...");
+
+        if (PaymentMethod == "BKASH")
+        {
+            // implement algorithm or api proccess here
+            Console.WriteLine("payment throw bkash.");
+        }
+        else if(PaymentMethod == "NAGAD")
+        {
+            // implement algorithm or api proccess here
+            Console.WriteLine("payment throw nagad.");
+        }
+        else if (PaymentMethod == "ROCKET")
+        {
+            // implement algorithm or api proccess here
+            Console.WriteLine("payment throw rocket.");
+        }
+    }
+}
---
+++ Strategy-Problem/Program.cs
@@ -0,0 +1,26 @@
+﻿using Strategy_Problem;
+
+
+PaymentGateway paymentGateway = new();
+
+// implement payment with bkash
+paymentGateway.Payment("BKASH");
+
+// implement payment with nagad
+paymentGateway.Payment("NAGAD");
+
+// implement payment with ROCKET
+paymentGateway.Payment("ROCKET");
+
+//what happen if we want to add UPAY
+/*
+ 
+    - it's violet open close priciple
+    - need to change existing code
+    - can be arise bug
+    - so on ....
+    - here is problem.. we can resolve it implement stategy pattern
+
+ */
+
+//.....paymentGateway.Payment("UPAY");
